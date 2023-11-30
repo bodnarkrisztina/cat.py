845c5b9841d754cd76b2fea014e2d79fe634642f
Kriszta <90178422+bodnarkrisztina@users.noreply.github.com>
2023-11-30 14:16:44 +0100

    Update cat.py

--- cat.py
+++ cat.py
@@ -6,8 +6,8 @@ import streamlit as st
 def load_data():
     return pd.DataFrame(
         {
-            "első rács": [1, 2, 3, 4],
-            "második rács": [10, 20, 30, 40],
+            "első rács": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            "második rács": [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
         }
     )
 
