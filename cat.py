4e04a2f27cfbd16ed1e9920631e6d9fbac917717
Kriszta <90178422+bodnarkrisztina@users.noreply.github.com>
2023-11-30 14:10:59 +0100

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,19 +1,27 @@
-import streamlit as st 
-layer1 = st.write("°","°","°","°","°","°","°","°","°","°","°"","0","°","°"","°","°","°","°")
-layer2 = st.write(
-layer3 = st.write(
-layer4 = st.write(
-layer5 = st.write(
-layer6 = st.write(
-layer7 = st.write(
-layer8 = st.write(
-layer9 = st.write(
-layer10 =st.write(
-layer11= st.write(
-layer12= st.write(
-layer13= st.write(
-layer14= st.write(
-layer15= st.write(
-layer16= st.write(
-layer17= st.write(
-layer18= st.write(
+import random
+import pandas as pd
+import streamlit as st
+
+df = pd.DataFrame(
+    {
+        "name": ["Roadmap", "Extras", "Issues"],
+        "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
+        "stars": [random.randint(0, 1000) for _ in range(3)],
+        "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
+    }
+)
+st.dataframe(
+    df,
+    column_config={
+        "name": "App name",
+        "stars": st.column_config.NumberColumn(
+            "Github Stars",
+            help="Number of stars on GitHub",
+            format="%d ⭐",
+        ),
+        "url": st.column_config.LinkColumn("App URL"),
+        "views_history": st.column_config.LineChartColumn(
+            "Views (past 30 days)", y_min=0, y_max=5000
+        ),
+    },
+    hide_index=True,
