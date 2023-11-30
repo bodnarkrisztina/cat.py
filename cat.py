605e4d647f45d65660d7f6217a6e783caa476e5c
Kriszta <90178422+bodnarkrisztina@users.noreply.github.com>
2023-11-30 13:58:02 +0100

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,2 +1,19 @@
-import streamlit as st
-layer = st.write("&nbsp","o","......","o","......") 
+import streamlit as st ⬜    ⬛
+layer1 = st.write("⬜","⬜""⬜","⬜","⬜","⬜","⬜","⬜","⬜","⬜","⬜","⬛","⬜","⬜","⬜","⬜","⬜","⬜")
+layer2 = st.write("⬜","⬜","⬜","⬛,"⬜","⬜","⬜","⬜","⬜","⬜","⬛","⬛","⬛","⬜","⬜","⬜","⬜","⬜")
+layer3 = st.write(
+layer4 = st.write(
+layer5 = st.write(
+layer6 = st.write(
+layer7 = st.write(
+layer8 = st.write(
+layer9 = st.write(
+layer10 =st.write(
+layer11= st.write(
+layer12= st.write(
+layer13= st.write(
+layer14= st.write(
+layer15= st.write(
+layer16= st.write(
+layer17= st.write(
+layer18= st.write(
